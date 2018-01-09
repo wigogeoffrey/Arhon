5876f2dd39397dbe949632dd359bcac1c9faa91f
Wigo Geoffrey <geoffrey@wigo-media.com>
2018-01-09 19:53:11 +0100

    change view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Arhon !
                 </div>
 
                 <div class="links">
